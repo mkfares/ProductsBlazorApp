55e1dcb0fd07921492d3f44c86224ba167c6709c
Mohamed Fares <24603443+mkfares@users.noreply.github.com>
2019-11-23 18:46:25 +0300

    Added database file

---
+++ ProductsBlazorApp/ProductsBalzorDatabase.sql
@@ -0,0 +1,40 @@
+﻿CREATE DATABASE ProductsBlazorDatabase;
+GO
+
+USE ProductsBlazorDatabase;
+GO
+
+CREATE TABLE Category(
+	CategoryId INT IDENTITY PRIMARY KEY,
+	[Name] NVARCHAR(max) NOT NULL
+);
+GO
+
+CREATE TABLE Product(
+	ProductId INT IDENTITY PRIMARY KEY,
+	[Name] NVARCHAR(max) NOT NULL,
+	Price DECIMAL(10,2) NOT NULL,
+	Quantity INT,
+	OrderDate DATETIME2,
+	Color INT,
+	IsChecked BIT,
+	CategoryId INT,
+	CONSTRAINT FK_Product_Category FOREIGN KEY (CategoryId) REFERENCES Category(CategoryId)
+);
+GO
+
+INSERT INTO Category VALUES 
+('Fruit'), 
+('Vegetable'),
+('Bread');
+GO
+
+INSERT INTO Product VALUES
+('Banana',	4.00,  30,	'2019-10-1', 1,	1, 1),
+('Pear',	8.00,  10,	'2019-11-1', 1,	0, 1),
+('Apple',	5.00,  50,	'2019-11-2', 2,	1, 1),
+('Orange',	6.00,  60,	'2019-11-5', 2,	0, 1),
+('Lettuce',	9.00, 100,	'2019-11-2', 3,	1, 2),
+('Tomato',	5.00,  50,	'2019-11-8', 3, 0, 2),
+('Potato',	2.00, 200,	'2019-11-9', 1, 1, 2);
+GO
